376b2fed3b97a5a84434512e5580aec2c43ea6c2
daecheverria <decheverria@correo.unimet.edu.ve>
2024-10-25 13:20:14 -0400

    Creacion

---
+++ ProyectoSO2/src/main/java/codigo/ProyectoSO2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package codigo;
+
+/**
+ *
+ * @author danie
+ */
+public class ProyectoSO2 {
+
+    public static void main(String[] args) {
+        System.out.println("Dioxido de Azufre");
+    }
+}
